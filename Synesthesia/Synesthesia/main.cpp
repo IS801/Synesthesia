48ae2c59c521c5d18176e0f23e19c63295951080
Isaac Shaw <isaac.shaw998@gmail.com>
2024-09-17 13:16:40 -0600

    Adding initial TriangleFan visualizer draft

---
+++ Synesthesia/Synesthesia/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Synesthesia
+//
+//  Created by Isaac Shaw on 9/17/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
