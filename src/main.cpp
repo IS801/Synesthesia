a11f825589bcc518ac0481a0a77018e999a8f91c
Isaac Shaw <isaac.shaw998@gmail.com>
2024-09-18 08:56:41 -0600

    adding wav files to repo

--- src/main.cpp
+++ src/main.cpp
@@ -24,14 +24,7 @@ public:
         m_vertices(VertexArray(TriangleFan, count)),
         m_lifetime(sf::seconds(30)),
         m_emitter(500, 500)
-    {
-//        colors =
-//    }
-//    
-//    void setColors(std::vector<sf::Color, colors){
-//        this->colors = colors;
-    }
-    
+    {}
 
     void setEmitter(sf::Vector2f position)
     {
@@ -46,21 +39,22 @@ public:
             int randomColorIndex = std::rand() % colors.size();
             // update the particle lifetime
             Particle& p = m_particles[i];
+            m_vertices[0].color = colors[0];
             p.lifetime -= elapsed;
             
             // if the particle is dead, respawn it
             if (p.lifetime <= sf::Time::Zero){
                 resetParticle(i);
-//                m_vertices[0].color = colors[0];
                 m_vertices[i].color = colors[randomColorIndex];
             }
 
             // update the position of the corresponding vertex
-            m_vertices[i].position += p.velocity * elapsed.asSeconds();
+            m_vertices[0].position += p.velocity;
+            m_vertices[i + 1].position += p.velocity * elapsed.asSeconds();
 
             // update the alpha (transparency) of the particle according to its lifetime
-            float ratio = p.lifetime.asSeconds() / m_lifetime.asSeconds();
-            m_vertices[i].color.a = static_cast<sf::Uint8>(ratio + 111 * 255);
+//            float ratio = p.lifetime.asSeconds() / m_lifetime.asSeconds();
+            m_vertices[i].color.a = static_cast<sf::Uint8>(/*ratio +*/ 450 * 255);
         }
     }
 
@@ -91,8 +85,11 @@ private:
         // give a random velocity and lifetime to the particle
         float angle = (std::rand() % 360) * 3.14f / 180.f;
         float speed = (std::rand() % 50) + 50.f;
-        m_particles[index].velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
-        m_particles[index].lifetime = sf::milliseconds((std::rand() % 6500) + 1000);
+        m_particles[index].velocity = sf::Vector2f(std::cos(angle) * speed * 2, std::sin(angle) * speed * 2);
+        m_particles[0].velocity = sf::Vector2f(std::cos(angle) * speed * 7, std::sin(angle) * speed * 7);
+        m_particles[index].lifetime = sf::milliseconds((std::rand() % 20000) + 1000);
+        m_particles[0].lifetime = sf::milliseconds((std::rand() % 99999999999999) + 1000);
+        
 
         // reset the position of the corresponding vertex
         m_vertices[index].position = m_emitter;
@@ -115,7 +112,7 @@ int main()
 
     RenderWindow window(VideoMode(1000, 1000), "Synesthesia", Style::Default, settings);
     
-    ParticleSystem particles(150);
+    ParticleSystem particles(75);
     Clock clock;
 
     // run the program as long as the window is open
